cd25e743785f3e49b1198d378ce7b48779336a4f
ouxingyuan <oddpro601@gmail.com>
2023-09-23 01:58:06 +0800

    feat: ✨ config github page

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
+	base: "https://gitname.github.io/RsapScroll3DGrid",
 	plugins: [react()],
 })
